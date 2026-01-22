2c22e24f77cf5de27291e462df9e731a1c00b01c
tli63 <tli63145@>
2026-01-23 03:34:46 +0800

    2026.1.23 3:18

--- demo/src/main/java/com/example/demo/domain/dto/StudentInfo.java
+++ demo/src/main/java/com/example/demo/domain/dto/StudentInfo.java
@@ -6,8 +6,10 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.example.demo.domain.entity.BaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @TableName("student_info")
 public class StudentInfo extends BaseEntity {
 
